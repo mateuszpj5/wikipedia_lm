ec06e7300ca58345a654c3d1c9bc05f6f1b42291
Mateusz Piatek <mateuszpj5@gmail.com>
2025-02-10 13:38:24 +0100

    modified articles_scraping.py; parts of the code not working in some cases, need of improving the html tree searching and the speed/memory efficiency

--- articles_scraping.py
+++ articles_scraping.py
@@ -6,12 +6,23 @@ URL = 'https://en.wikipedia.org'
 
 
 with open('lvl_paths.txt', 'r') as file:
-    raw = file.readlines()
+    paths = file.readlines()
 
-
-for line in raw:
+hrefs = []
+for line in paths:
     html = requests.get(URL + line.translate({ord('\n'): None})).text
     soup = BeautifulSoup(html, 'html.parser')
+    divs = soup.find_all('div', attrs={'class': 'div-col'})
+    for div in divs:
+        lists = div.find_all('ol')
+        for ol in lists:
+            for child in ol.children:
+                contents = child.find_all(['a', 'b'])
+                for item in contents:
+                    if item.has_attr('class'):
+                        contents.remove(item)
+                for item in contents:
+                     hrefs.append(item.href)
 
-    lists = soup.find_all('div', attrs={'class': 'div-col'})
+
     
